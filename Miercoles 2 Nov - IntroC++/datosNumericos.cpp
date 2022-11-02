0e53247ad25746c158d23d004b9f50638b670244
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2022-11-02 10:42:22 -0600

    Codigo inicial

---
+++ Miercoles 2 Nov - IntroC++/datosNumericos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std; //evita uso de std::cout
+
+// Demostración de uso de tipos numéricos
+int main(){
+
+    cout << (2+3*4*5) << endl;
+    cout << (2+3)*5 << endl;
+    cout << pow(2, 5) << endl;//Elevar potencia
+    cout << float(9)/3 << endl;//División flotante
+    cout << float(5)/3 << endl;
+    cout << 11/3 << endl; //Resultado es int
+    cout << 11%3 << endl; //Módulo normal
+    cout << float(4)/6 << endl;
+    cout << 4/6 << endl; //parte entera
+    cout << 4%6 << endl; //residuo
+    cout << pow(2, 150) << endl; //científica
+
+    return 0;
+}
---
+++ Miercoles 2 Nov - IntroC++/helloWorld.cpp
@@ -0,0 +1,8 @@
+// El famoso Hello World
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hola Mundo" << std::endl;
+    return 0;
+}
